7ac8c271f312d8040254a5709d21c52d30a83ba8
jmartinezdejuan <jmartinezdejuan@gmail.com>
2018-05-28 20:23:39 +0200

    Initial Commit

---
+++ AguantARTests/AguantARTests.swift
@@ -0,0 +1,36 @@
+//
+//  AguantARTests.swift
+//  AguantARTests
+//
+//  Created by Jesus M Martínez de Juan on 28/5/18.
+//  Copyright © 2018 CHECHU. All rights reserved.
+//
+
+import XCTest
+@testable import AguantAR
+
+class AguantARTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
